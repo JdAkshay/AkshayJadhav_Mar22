9cdf874c667ad6dc70850e085b348234da7bc499
JdAkshay <100847648+JdAkshay@users.noreply.github.com>
2022-03-10 15:07:43 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Assignment2
+{
+	public static void main (String args[])
+	{
+		System.out.println("Enter Number: ");
+		int i=Sc.nextInt();
+		if( i==2 || i = n1/2)
+		{
+		System.out.println("composite");
+		}
+		
+		if(n1%i == 0)
+		{
+		System.out.println("Prime");
+		
+		}
+	}	
+}
